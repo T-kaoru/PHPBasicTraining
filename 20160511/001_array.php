03548531c2a66d076c3d9e302f1ce8891239a847
T-kaoru <kkk73037@outlook.jp>
2016-05-11 10:32:09 +0900

    20160510v2

---
+++ 20160511/001_array.php
@@ -0,0 +1,11 @@
+<?php
+
+$br = "<br>";
+
+$a["PHP"] = $br . "入門" . $br;
+$a["C言"] = "語" . $br;
+$a["VPS"] = "比較";
+echo $br;
+print_r ($a);
+echo $br . $a["C言"];
+?>
---
+++ 20160511/004_array4.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = array(
+    0 => array(
+        0 => 'AA',
+        1 => 'AB',
+    ),
+    1 => array(
+        0 => 'BA',
+        1 => 'BB',
+        2 => 'BC',
+    ),
+);
+
+var_dump($a);
+echo "<br>";
+
+foreach ($a as $key1 => $value1) {
+    foreach ($a[$key1] as $key2 => $value2) {
+        echo "<br>" . $value2;
+    }
+}
+
+?>
